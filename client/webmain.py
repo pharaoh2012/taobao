353e9ea14aaa56b7aff6fca50404fb9e24eef7fa
raoming <raoming@dcqtech.com>
2013-05-22 14:08:49 +0800

    2013-05-22 周三 14:08:49.06

--- client/webmain.py
+++ client/webmain.py
@@ -26,14 +26,11 @@ def index():
 def index_page(page='1'):
     page = int(page)
     return showPage(page)
-    # return static_file("reload.html", root="./js")
 
 
 def showPage(page):
     itemperpage = 16
     db = Store(SQLiteWriter("taobao.db", frozen=True))
-    # items = mongo_db.tabaotry.find().sort([("date", pymongo.DESCENDING), (
-    #     "gailv", pymongo.ASCENDING)]).skip((page-1)*itemperpage).limit(itemperpage)
     rs = []
     items = db.find("freetry", "1 order by date desc,gailv  limit %s,%s" %
                     ((page - 1) * itemperpage, itemperpage))
@@ -50,4 +47,3 @@ def taobao(url):
 
 run(app, host='127.0.0.1', port=7702, server="auto",
     debug=True, reloader=True, quiet=False)
-# run(host='localhost', port=7702, debug=True, reloader=True, quiet=False)
